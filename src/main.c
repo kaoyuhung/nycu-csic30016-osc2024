0dfe2d02cf89415a7208f4aa81a32d0f1c62d7b1
yh_Kao <rb910518@gmail.com>
2024-03-29 14:28:13 +0800

    commit

--- src/main.c
+++ src/main.c
@@ -1,27 +1,23 @@
-#include "mbox.h"
 #include "shell.h"
 #define CMD_LEN 128
 
-enum shell_status {
-    Read,
-    Parse
-};
+enum shell_status { Read, Parse };
 
 int main() {
-    shell_init();
-    enum shell_status status = Read;
-    while (1) {
-        char cmd[CMD_LEN];
-        switch (status) {
-        case Read:
-            shell_input(cmd);
-            status = Parse;
-            break;
+  shell_init();
+  enum shell_status status = Read;
+  while (1) {
+    char cmd[CMD_LEN];
+    switch (status) {
+      case Read:
+        shell_input(cmd);
+        status = Parse;
+        break;
 
-        case Parse:
-            shell_controller(cmd);
-            status = Read;
-            break;
-        }
+      case Parse:
+        shell_controller(cmd);
+        status = Read;
+        break;
     }
+  }
 }
